fdf473bb9fa660f650f454bb34de179760079c16
Tieirra <167941085+Tieirra77@users.noreply.github.com>
2024-06-01 17:44:53 -0500

    creating to do list

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hi my name is Tieirra Hi 
+     <h1> My to do List</h1>
+     <div>
+      <h2> Finish Frontend simplifed </h2>
+      <button> Delete</button>
+     </div>
     </div>
   );
 }
